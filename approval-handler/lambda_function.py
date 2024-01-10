629353d986d9a8ddad059d93a3e1d8c67de39645
Vaibhav Pandey <contact@vaibhavpandey.com>
2024-01-10 13:57:34 +0530

    Little more formatting.

--- approval-handler/lambda_function.py
+++ approval-handler/lambda_function.py
@@ -7,32 +7,47 @@ from urllib.parse import parse_qs
 
 SLACK_VERIFICATION_TOKEN = os.environ["SLACK_VERIFICATION_TOKEN"]
 
+
 def lambda_handler(event, context):
     # for debugging only
     # print("received event:", json.dumps(event, indent=2))
 
-    query_str = base64.b64decode(event["body"]).decode("utf-8") if event["isBase64Encoded"] else event["body"]
+    query_str = (
+        base64.b64decode(event["body"]).decode("utf-8")
+        if event["isBase64Encoded"]
+        else event["body"]
+    )
 
     body = parse_qs(query_str)
     payload = json.loads(body["payload"][0])
 
     if SLACK_VERIFICATION_TOKEN != payload["token"]:
         return {
-            "isBase64Encoded": "false",
+            "isBase64Encoded": False,
             "statusCode": 403,
-            "body": "The security token does not match SLACK_VERIFICATION_TOKEN."
+            "body": "The security token does not match SLACK_VERIFICATION_TOKEN.",
         }
 
     details = json.loads(payload["actions"][0]["value"])
     send_approval_to_aws(details)
 
-    msg = "*{}* action for <{}|{}> was :white_check_mark: approved by <@{}>." if details["approve"] else "*{}* action for <{}|{}> was :x: denied by <@{}>"
+    msg = (
+        "*{}* action for <{}|{}> was :white_check_mark: approved by <@{}>."
+        if details["approve"]
+        else "*{}* action for <{}|{}> was :x: denied by <@{}>"
+    )
 
     return {
-        "isBase64Encoded": "false",
+        "isBase64Encoded": False,
         "statusCode": 200,
-        "body": msg.format(details["actionName"], details["consoleLink"], details["codePipelineName"], payload["user"]["id"]),
+        "body": msg.format(
+            details["actionName"],
+            details["consoleLink"],
+            details["codePipelineName"],
+            payload["user"]["id"],
+        ),
     }
+
 
 def send_approval_to_aws(action_details):
     codepipeline_status = "Approved" if action_details["approve"] else "Rejected"
@@ -43,11 +58,12 @@ def send_approval_to_aws(action_details):
     client = boto3.client("codepipeline")
 
     response_approval = client.put_approval_result(
-										pipelineName=codepipeline_name,
-										stageName=stage_name,
-										actionName=action_name,
-										result={"summary": "", "status": codepipeline_status},
-										token=token)
+        pipelineName=codepipeline_name,
+        stageName=stage_name,
+        actionName=action_name,
+        result={"summary": "", "status": codepipeline_status},
+        token=token,
+    )
 
     # for debugging only
     # print("approval response:", response_approval)
--- slack-notification/lambda_function.py
+++ slack-notification/lambda_function.py
@@ -5,6 +5,7 @@ from urllib.request import Request, urlopen
 
 SLACK_WEBHOOK_URL = os.environ["SLACK_WEBHOOK_URL"]
 
+
 def lambda_handler(event, context):
     # for debugging only
     # print("received event:", json.dumps(event, indent=2))
@@ -19,7 +20,9 @@ def lambda_handler(event, context):
     stage_name = data["approval"]["stageName"]
 
     slack_message = {
-        "text": "*{}* action for <{}|{}> is awaiting approval.".format(action_name, console_link, codepipeline_name),
+        "text": "*{}* action for <{}|{}> is awaiting approval.".format(
+            action_name, console_link, codepipeline_name
+        ),
         "attachments": [
             {
                 "text": "Confirm whether to continue or not.",
@@ -33,23 +36,41 @@ def lambda_handler(event, context):
                         "text": "Confirm",
                         "style": "danger",
                         "type": "button",
-                        "value": json.dumps({"actionName": action_name, "approve": True, "consoleLink": console_link, "codePipelineToken": token, "codePipelineName": codepipeline_name, "stageName": stage_name}),
+                        "value": json.dumps(
+                            {
+                                "actionName": action_name,
+                                "approve": True,
+                                "consoleLink": console_link,
+                                "codePipelineToken": token,
+                                "codePipelineName": codepipeline_name,
+                                "stageName": stage_name,
+                            }
+                        ),
                         "confirm": {
                             "title": "Do you really want to do this?",
-                            "text": "This will continue the execution of " + codepipeline_name + " pipeline.",
+                            "text": "This will continue the execution of {} pipeline.".format(codepipeline_name),
                             "ok_text": "Confirm",
-                            "dismiss_text": "Cancel"
-                        }
+                            "dismiss_text": "Cancel",
+                        },
                     },
                     {
                         "name": "deployment",
                         "text": "Cancel",
                         "type": "button",
-                        "value": json.dumps({"actionName": action_name, "approve": False, "consoleLink": console_link, "codePipelineToken": token, "codePipelineName": codepipeline_name, "stageName": stage_name})
-                    }
-                ]
+                        "value": json.dumps(
+                            {
+                                "actionName": action_name,
+                                "approve": False,
+                                "consoleLink": console_link,
+                                "codePipelineToken": token,
+                                "codePipelineName": codepipeline_name,
+                                "stageName": stage_name,
+                            }
+                        ),
+                    },
+                ],
             }
-        ]
+        ],
     }
 
     req = Request(SLACK_WEBHOOK_URL, json.dumps(slack_message).encode("utf-8"))
